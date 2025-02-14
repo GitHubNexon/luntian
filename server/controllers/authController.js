3d9aae1cd9bcdad4b48342b4663a4c99852e02e4
GitHubNexon <johnmarkpulmano052802@gmail.com>
2025-02-15 02:15:59 +0800

    change bcrypt to bcryptjs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const User = require("../models/userModel"); // Adjust this path based on your folder structure
